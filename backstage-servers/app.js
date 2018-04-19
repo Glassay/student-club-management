a161b1e3e1253763863b2b3ec9a6a59269b118c3
Glassay <31647920+Glassay@users.noreply.github.com>
2018-04-19 14:35:50 +0800

    建表初步完成

--- backstage-servers/app.js
+++ backstage-servers/app.js
@@ -5,12 +5,12 @@
 
 const knex = require('knex')({
   client: 'mysql',
-  connection: {
-    host: '127.0.0.1',
-    user: 'root',
-    password: '111111',
-    database: 'studentClub',
-  },
+  // connection: {
+  //   host: '127.0.0.1',
+  //   user: 'root',
+  //   password: '111111',
+  //   database: 'studentClub',
+  // },
 });
 
 module.exports = app => {
@@ -19,11 +19,40 @@ module.exports = app => {
     if (hasUser.length === 0) {
       const studentClub = knex.schema.createTableIfNotExists('User', function(table) {
         table.increments();
-        table.string('user').notNullable().defaultTo('');
+        table.string('username').notNullable().defaultTo('');
         table.string('password').notNullable().defaultTo('');
-        table.string('header').notNullable().defaultTo('https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png');
+        table.string('jurisdiction').notNullable().defaultTo('');
         table.timestamp('create_at').defaultTo(knex.fn.now());
         table.charset('utf8');
+      });
+      yield app.mysql.query(studentClub.toString());
+    }
+
+    const hasClub = yield app.mysql.query(knex.schema.hasTable('Club').toString());
+    if (hasClub.length === 0) {
+      const studentClub = knex.schema.createTableIfNotExists('Club', function(table) {
+        table.increments();
+        table.string('title').notNullable().defaultTo('');
+        table.string('content').notNullable().defaultTo('');
+        table.string('type').notNullable().defaultTo('');
+        table.boolean('status').notNullable().defaultTo(false);
+        table.integer('budget').notNullable().defaultTo(0);
+        table.string('club').notNullable().defaultTo('');
+        table.timestamp('create_at').defaultTo(knex.fn.now());
+        table.charset('utf8');
+      });
+      yield app.mysql.query(studentClub.toString());
+    }
+
+    const hasMember = yield app.mysql.query(knex.schema.hasTable('Member').toString());
+    if (hasMember.length === 0) {
+      const studentClub = knex.schema.createTableIfNotExists('Member', function(table) {
+        table.integer('studentNumber').notNullable().defaultTo(0);
+        table.string('name').notNullable().defaultTo('');
+        table.string('sex').notNullable().defaultTo('');
+        table.string('class').notNullable().defaultTo('');
+        table.string('club').notNullable().defaultTo('');
+        table.integer('level').notNullable().defaultTo(0);
       });
       yield app.mysql.query(studentClub.toString());
     }
