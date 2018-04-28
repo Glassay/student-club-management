a491d8c9dee311d7c7400675079f1af8d2569c29
Glassay <31647920+Glassay@users.noreply.github.com>
2018-04-28 20:07:57 +0800

    修改请求名

--- backstage-servers/app/router.js
+++ backstage-servers/app/router.js
@@ -8,7 +8,7 @@ module.exports = app => {
   router.get('/', controller.home.index);
   router.post('/login', 'users.login');
   router.get('/all', 'users.info');
-  router.post('/members', 'members.getInfo');
+  router.post('/getMembers', 'members.getInfo');
   router.post('/deleteMembers', 'members.deleteInfo');
   router.post('/updateMembers', 'members.updateInfo');
   router.post('/insertMembers', 'members.insertInfo');
